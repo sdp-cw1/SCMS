026935f9a5b38ee896f456ba637946f0708008dc
Nilan Chathura <nilanchathura8@gmail.com>
2025-03-09 12:59:06 +0530

    feat: add calendar controller

---
+++ SCMS/Controllers/CalendarController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SCMS.Controllers
+{
+    public class CalendarController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
