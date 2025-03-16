1b68c964c00b8f06315e09decda9a153c78fa951
dhanu perera <dhanut@tuta.io>
2025-03-16 21:06:59 +0530

    !feat: adding user creating functions
    - breaking changes: haven't tested it

--- SCMS/Models/DBModel.cs
+++ SCMS/Models/DBModel.cs
@@ -1,18 +1,16 @@
-using System;
-using Microsoft.Extensions.Configuration;
 using MySqlConnector;
 
 namespace SCMS.Models
 {
     public class DBModel
     {
-        private readonly string _connectionString;
+        private readonly string _connectionString = "Server=127.0.0.1;User ID=root;Password=password;Database=scms";
 
         // Constructor to inject IConfiguration
-        public DBModel(IConfiguration configuration)
+        /* public DBModel(IConfiguration configuration)
         {
             _connectionString = configuration.GetConnectionString("scmsCon");
-        }
+        }*/
 
         public bool IsValidUser(string email, string password)
         {
@@ -27,12 +25,93 @@ namespace SCMS.Models
 
             string hashedPassword = Convert.ToString(cmd.ExecuteScalar());
 
-            if (hashedPassword.Length == 0)
+            return BCrypt.Net.BCrypt.Verify(text: password, hash: hashedPassword);
+
+            // int userCount = Convert.ToInt32(cmd.ExecuteScalar());
+            // return userCount > 0;
+        }
+
+        public bool CreateAdminUser(string username, string nic, string email, string phone, string password)
+        {
+            using var connection = new MySqlConnection(_connectionString);
+            connection.Open();
+
+            var hashedPassword = BCrypt.Net.BCrypt.HashPassword(password);
+
+            string query = "CALL InsertUserAndAdmin(@Username, @Nic, @Email, @Phone, @Password)";
+
+            using var cmd = new MySqlCommand(query, connection);
+            cmd.Parameters.AddWithValue("@Username", username);
+            cmd.Parameters.AddWithValue("@Nic", nic);
+            cmd.Parameters.AddWithValue("@Email", email);
+            cmd.Parameters.AddWithValue("@Phone", phone);
+            cmd.Parameters.AddWithValue("@Password", hashedPassword);
+            try
+            {
+                cmd.ExecuteReader();
+                return true;
+            }
+            catch (System.Exception)
+            {
+                return false;
+            }
+        }
+
+        public bool CreateAccStaffUser(string username, string nic, string email, string phone, string password, DateOnly dob, string firstName, string lastName) {
+            using var connection = new MySqlConnection(_connectionString);
+            connection.Open();
+
+            var hashedPassword = BCrypt.Net.BCrypt.HashPassword(password);
+
+            string query = "CALL InsertUserAndAcc(@Username, @Nic, @Email, @Phone, @Password, @Dob, @FirstName, @LastName)";
+
+            using var cmd = new MySqlCommand(query, connection);
+            cmd.Parameters.AddWithValue("@Username", username);
+            cmd.Parameters.AddWithValue("@Nic", nic);
+            cmd.Parameters.AddWithValue("@Email", email);
+            cmd.Parameters.AddWithValue("@Phone", phone);
+            cmd.Parameters.AddWithValue("@Password", hashedPassword);
+            cmd.Parameters.AddWithValue("@Dob", dob);
+            cmd.Parameters.AddWithValue("@FirstName", firstName);
+            cmd.Parameters.AddWithValue("@LastName", lastName);
+            try
+            {
+                cmd.ExecuteReader();
+                return true;
+            }
+            catch (System.Exception)
+            {
+                return false;
+            }
+        }
+
+        public bool CreateStudentUser(string username, string nic, string email, string phone, string password, DateOnly dob, string firstName, string lastName) {
+            using var connection = new MySqlConnection(_connectionString);
+            connection.Open();
+
+            var hashedPassword = BCrypt.Net.BCrypt.HashPassword(password);
+
+            string query = "CALL InsertUserAndStu(@Username, @Nic, @Email, @Phone, @Password, @Dob, @FirstName, @LastName)";
+
+            using var cmd = new MySqlCommand(query, connection);
+            cmd.Parameters.AddWithValue("@Username", username);
+            cmd.Parameters.AddWithValue("@Nic", nic);
+            cmd.Parameters.AddWithValue("@Email", email);
+            cmd.Parameters.AddWithValue("@Phone", phone);
+            cmd.Parameters.AddWithValue("@Password", hashedPassword);
+            cmd.Parameters.AddWithValue("@Dob", dob);
+            cmd.Parameters.AddWithValue("@FirstName", firstName);
+            cmd.Parameters.AddWithValue("@LastName", lastName);
+            try
+            {
+                cmd.ExecuteReader();
+                return true;
+            }
+            catch (System.Exception)
             {
                 return false;
             }
 
-            return BCrypt.Net.BCrypt.Verify(text: password, hash: hashedPassword);
         }
     }
 }
