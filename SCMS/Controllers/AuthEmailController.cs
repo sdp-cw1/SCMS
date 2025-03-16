5594d193c99650efb46e2770735fbb93eb47ccbc
Ganidu Jayasanka <ganidujayasanka@gmail.com>
2025-03-16 22:23:49 +0530

    FIX: Validate email and pwd in db table

--- SCMS/Controllers/AuthEmailController.cs
+++ SCMS/Controllers/AuthEmailController.cs
@@ -1,9 +1,11 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SCMS.Models;
 
 namespace SCMS.Controllers
 {
     public class AuthEmailController : Controller
     {
+
         public IActionResult Index()
         {
             return View();
@@ -12,9 +14,17 @@ namespace SCMS.Controllers
         [HttpPost]
         public IActionResult Authenticate(string email)
         {
-            if (!string.IsNullOrEmpty(email))
-            {
+
+           // if (!string.IsNullOrEmpty(email))
+            //{
                 // Here, you can add logic to check the email in the database
+            //    return RedirectToAction("Index", "AuthPassword"); // Redirect to Dashboard
+             //            }
+
+            bool isValidEmail = new DBModel().IsValidEmail(email);
+
+            if (isValidEmail)
+            {
                 return RedirectToAction("Index", "AuthPassword"); // Redirect to Dashboard
             }
 
