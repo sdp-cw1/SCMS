9a92c8b166711260615a6b77e9c558526e592707
dhanu perera <dhanut@tuta.io>
2025-03-19 18:30:07 +0530

    feat: get and add classrooms to the db

---
+++ SCMS/Models/DBClassroom.cs
@@ -0,0 +1,60 @@
+using MySqlConnector;
+
+namespace SCMS.Models
+{
+    public class DBClassrooms
+    {
+
+        private readonly string _connectionString;       
+        private readonly MySqlConnection connection;
+
+        // Constructor to inject IConfiguration
+        public DBClassrooms()
+        {
+            // _connectionString = configuration.GetConnectionString("scmsCon");
+            _connectionString  = "Server=127.0.0.1;User ID=root;Password=Gmysql#4321;Database=scms";
+            connection = new MySqlConnection(_connectionString);
+
+        }
+
+        public bool InsertClassroom(string id, string name, int seats)
+        {
+            string query = "INSERT INTO classrooms VALUES (@Id, @Name, @Seats)";
+            using var cmd = new MySqlConnector.MySqlCommand(query, connection);
+            cmd.Parameters.AddWithValue("@Id", id);
+            cmd.Parameters.AddWithValue("@Name", name);
+            cmd.Parameters.AddWithValue("@Seats", seats);
+
+            try
+            {
+                cmd.ExecuteScalar();
+                return true;
+            }
+            catch (System.Exception)
+            {
+                return false;
+            }
+        }
+
+        public List<ClassRoomModel> GetClassRooms()
+        {
+            List<ClassRoomModel> lst = new List<ClassRoomModel>();
+
+            string query = "SELECT * FROM classrooms";
+            using var cmd = new MySqlConnector.MySqlCommand(query, connection);
+
+            using var reader = cmd.ExecuteReader();
+
+            while(reader.Read())
+            {
+                lst.Add(new ClassRoomModel(
+                            reader.GetString("id"),
+                            reader.GetString("name"),
+                            int.Parse(reader.GetInt64("seats").ToString(), System.Globalization.CultureInfo.CurrentCulture)
+                ));
+            }
+
+            return lst;
+        }
+    }
+}
